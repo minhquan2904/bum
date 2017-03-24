77856c077410965b8dff772cef0d4c47043ee879
QLam <QLam>
2017-03-24 10:32:05 +0700

    fix update 2

--- js/customize.js
+++ js/customize.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+    new WOW().init();
 	if ($(window).width() > 767) {
 
                 var $sidebar   = $(".sidebar"), 
                     $window    = $(window),
                     offset     = $sidebar.offset(),
                     topPadding = 20;
-                    
+                
                 $window.scroll(function() {
                      if ($window.scrollTop() > offset.top) {
                         $sidebar.stop().animate({
