13c09651af8c54ea8d846465e2db476c86121fa1
minhquan2904 <minhquan2904@outlook.com>
2017-03-27 00:11:28 +0700

    regist

--- js/customize.js
+++ js/customize.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     new WOW().init();
 	if ($(window).width() > 767) {
 
-                var $sidebar   = $(".sidebar"), 
+                var $sidebar   = $(".sidebar"),
                     $window    = $(window),
                     offset     = $sidebar.offset(),
                     topPadding = 20;
-                
+
                 $window.scroll(function() {
                      if ($window.scrollTop() > offset.top) {
                         $sidebar.stop().animate({
@@ -21,4 +21,69 @@ $(document).ready(function(){
 
             };
 
+});
+$(function () {
+    $('.button-checkbox').each(function () {
+
+        // Settings
+        var $widget = $(this),
+            $button = $widget.find('button'),
+            $checkbox = $widget.find('input:checkbox'),
+            color = $button.data('color'),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+
+        // Event Handlers
+        $button.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $button.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $button.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$button.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $button
+                    .removeClass('btn-default')
+                    .addClass('btn-' + color + ' active');
+            }
+            else {
+                $button
+                    .removeClass('btn-' + color + ' active')
+                    .addClass('btn-default');
+            }
+        }
+
+        // Initialization
+        function init() {
+
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($button.find('.state-icon').length == 0) {
+                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i>');
+            }
+        }
+        init();
+    });
 });
